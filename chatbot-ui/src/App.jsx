095072a6f26001436627b438071540bbebe5bd82
diyadadheech <diya05dhadeech@gmail.com>
2025-11-22 16:24:57 +0530

    initial commit

--- chatbot-ui/src/App.jsx
+++ chatbot-ui/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     setMessages(newMessages);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("https://inkle-backend.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: input }),
